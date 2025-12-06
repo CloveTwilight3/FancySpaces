44b8f177ad235e7f1b45a758c3307812e4c0bfc0
Oliver <oschi1303@gmail.com>
2025-12-06 19:16:23 +0100

    Move version file handler to its own file

--- src/internal/versions/handler/versions_handler.go
+++ src/internal/versions/handler/versions_handler.go
@@ -81,35 +81,6 @@ func (h *Handler) handleVersion(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *Handler) handleVersionFile(w http.ResponseWriter, r *http.Request) {
-	sid := r.PathValue("space_id")
-	if sid == "" {
-		problems.ValidationError("space_id", "Space ID is required").WriteToHTTP(w)
-		return
-	}
-
-	vid := r.PathValue("version_id")
-	if vid == "" {
-		problems.ValidationError("version_id", "Version ID or name is required").WriteToHTTP(w)
-		return
-	}
-
-	fileName := r.PathValue("file_name")
-	if fileName == "" {
-		problems.ValidationError("file_name", "File name is required").WriteToHTTP(w)
-		return
-	}
-
-	switch r.Method {
-	case http.MethodGet:
-		h.handleDownloadVersionFile(w, r, sid, vid, fileName)
-	case http.MethodPost:
-		h.handleUploadVersionFile(w, r, sid, vid, fileName)
-	default:
-		problems.MethodNotAllowed(r.Method, []string{http.MethodGet, http.MethodPost}).WriteToHTTP(w)
-	}
-}
-
 // no auth required
 func (h *Handler) handleGetVersions(w http.ResponseWriter, r *http.Request, spaceID string) {
 	all, err := h.store.GetAll(r.Context(), spaceID)
@@ -241,82 +212,4 @@ func (h *Handler) handleDeleteVersion(w http.ResponseWriter, r *http.Request, sp
 	}
 
 	w.WriteHeader(http.StatusNoContent)
-}
-
-func (h *Handler) handleUploadVersionFile(w http.ResponseWriter, r *http.Request, spaceID, versionID, fileName string) {
-	u := h.userFromCtx(r.Context())
-	if u == nil || !u.Verified || !u.IsActive {
-		problems.Unauthorized().WriteToHTTP(w)
-		return
-	}
-
-	space, err := h.spaces.Get(spaceID)
-	if err != nil {
-		if errors.Is(err, spaces.ErrSpaceNotFound) {
-			problems.NotFound("Space", spaceID).WriteToHTTP(w)
-			return
-		}
-
-		slog.Error("Failed to get space by id", sloki.WrapError(err))
-		problems.InternalServerError("").WriteToHTTP(w)
-		return
-	}
-
-	if !space.HasWriteAccess(u) {
-		problems.Forbidden().WriteToHTTP(w)
-		return
-	}
-
-	ver, err := h.store.Get(r.Context(), spaceID, versionID)
-	if err != nil {
-		slog.Error("Failed to get version", sloki.WrapError(err))
-		problems.InternalServerError("").WriteToHTTP(w)
-		return
-	}
-
-	data := make([]byte, r.ContentLength)
-	_, err = r.Body.Read(data)
-	if err != nil {
-		slog.Error("Failed to read file data", sloki.WrapError(err))
-		problems.InternalServerError("").WriteToHTTP(w)
-		return
-	}
-
-	if err := h.store.UploadVersionFile(r.Context(), ver, fileName, data); err != nil {
-		slog.Error("Failed to upload version file", sloki.WrapError(err))
-		problems.InternalServerError("").WriteToHTTP(w)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-// no auth required
-func (h *Handler) handleDownloadVersionFile(w http.ResponseWriter, r *http.Request, spaceID, versionID, fileName string) {
-	if versionID == "latest" {
-		channel := r.URL.Query().Get("channel")
-		if channel == "" {
-			channel = "release"
-		}
-
-		ver, err := h.store.GetLatest(r.Context(), spaceID, channel)
-		if err != nil {
-			slog.Error("Failed to get latest version", sloki.WrapError(err))
-			problems.InternalServerError("").WriteToHTTP(w)
-			return
-		}
-		versionID = ver.ID
-	}
-
-	data, err := h.store.DownloadVersionFile(r.Context(), r, spaceID, versionID, fileName)
-	if err != nil {
-		slog.Error("Failed to download version file", sloki.WrapError(err))
-		problems.InternalServerError("").WriteToHTTP(w)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Header().Set("Content-Disposition", "attachment; filename=\""+fileName+"\"")
-	w.Header().Set("Cache-Control", "public, 86400") // 24h
-	w.Write(data)
 }
